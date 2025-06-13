a92e7a71eed8bbfba6c04718e98dcf2d1b9cd0fc
Nick-88-H <169513221+Nick-88-H@users.noreply.github.com>
2025-06-13 17:19:55 +0800

    Add custom keymap for Keychron K5 Max ANSI white with dynamic layers and sticky modifier support

---
+++ keyboards/keychron/k5_max/ansi/white/keymaps/Nick88H/keymap.c
@@ -0,0 +1,238 @@
+/* Copyright 2025 @ Keychron (https://www.keychron.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include QMK_KEYBOARD_H
+#include "keychron_common.h"
+#include "print.h"
+#include "action_tapping.h"
+
+enum layers {
+    MAC_BASE,
+    MAC_FN,
+    WIN_BASE,
+    WIN_FN,
+    WIN_HALF_QWERTY,
+};
+// ─────────────────────────── Keymaps ───────────────────────────
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+[MAC_BASE] = LAYOUT_108_ansi(
+        KC_ESC,             KC_BRID,  KC_BRIU,  KC_MCTRL, KC_LNPAD, BL_DOWN,  BL_UP,    KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU, KC_SNAP,  KC_SIRI,  BL_STEP, KC_F13,  KC_F14,  KC_F15,  KC_F16,
+        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC, KC_INS,   KC_HOME,  KC_PGUP, KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS, KC_DEL,   KC_END,   KC_PGDN, KC_P7,   KC_P8,   KC_P9,
+        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,                               KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
+        KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,           KC_UP,             KC_P1,   KC_P2,   KC_P3,
+        KC_LCTL,  KC_LOPTN, KC_LCMMD,                               KC_SPC,                                 KC_RCMMD, KC_ROPTN,MO(MAC_FN),KC_RCTL, KC_LEFT,  KC_DOWN,  KC_RGHT, KC_P0,            KC_PDOT, KC_PENT),
+
+    [MAC_FN] = LAYOUT_108_ansi(
+        _______,            KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,  _______,  _______,  BL_TOGG, _______, _______, _______, _______,
+        _______,  BT_HST1,  BT_HST2,  BT_HST3,  P2P4G,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______, _______,  _______,  _______, _______, _______, _______, _______,
+        BL_TOGG,  BL_STEP,  BL_UP,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______, _______,  _______,  _______, _______, _______, _______,
+        _______,  _______,  BL_DOWN,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,                              _______, _______, _______, _______,
+        _______,            _______,  _______,  _______,  _______,  BAT_LVL,  NK_TOGG,  _______,  _______,  _______,  _______,            _______,           _______,           _______, _______, _______,
+        _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______, _______,  _______,  _______, _______,          _______, _______),
+
+    [WIN_BASE] = LAYOUT_108_ansi(
+        KC_ESC,             KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,  KC_PSCR,  KC_CTANA, BL_STEP, _______, _______, _______, LGUI(KC_H),
+        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC, KC_INS,   KC_HOME,  KC_PGUP, KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS, KC_DEL,   KC_END,   KC_PGDN, KC_P7,   KC_P8,   KC_P9,
+        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,                               KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
+        OSM(MOD_LSFT),      KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,           KC_UP,             KC_P1,   KC_P2,   KC_P3,
+        KC_LCTL,  KC_LWIN,  KC_LALT,                                LT(WIN_HALF_QWERTY, KC_SPC),            KC_RALT,  KC_RWIN, MO(WIN_FN),KC_RCTL, KC_LEFT,  KC_DOWN,  KC_RGHT, KC_P0,            KC_PDOT, KC_PENT),
+
+    [WIN_FN] = LAYOUT_108_ansi(
+        _______,            KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,   BL_DOWN,  BL_UP,   KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU, _______,  _______,  BL_TOGG, _______, _______, _______, _______,
+        _______,  BT_HST1,  BT_HST2,  BT_HST3,  P2P4G,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______, _______,  _______,  _______, _______, _______, _______, _______,
+        BL_TOGG,  BL_STEP,  BL_UP,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______, _______,  _______,  _______, _______, _______, _______,
+        _______,  _______,  BL_DOWN,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,                              _______, _______, _______, _______,
+        _______,            _______,  _______,  _______,  _______,  BAT_LVL,  NK_TOGG,  _______,  _______,  _______,  _______,            _______,           _______,           _______, _______, _______,
+        _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______, _______,  _______,  _______, _______,          _______, _______),
+
+    [WIN_HALF_QWERTY] = LAYOUT_108_ansi(
+        _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______, _______,  _______,  _______, _______, _______, _______, _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     _______,  _______,  _______, _______,  _______,  _______, _______, _______, _______, _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     _______,  _______,  _______, _______,  _______,  _______, _______, _______, _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,               _______,                              _______, _______, _______, _______,
+        _______,            _______,  _______,  _______,  _______,  KC_Z,     KC_X,     KC_C,     KC_V,     _______,  _______,            _______,           _______,           _______, _______, _______,
+        _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______, _______,  _______,  _______, _______,          _______, _______),
+
+};
+
+// ─────────────── Sticky Modifier Handling ───────────────
+#define MODIFIER_TIMEOUT    5000
+#define IS_OSM_SHIFT(kc) ((kc) == OSM(MOD_RSFT))
+
+#define IS_REAL_MOD(kc) \
+    ((kc) == KC_RCTL || (kc) == KC_RALT || \
+     (kc) == KC_RSFT || (kc) == KC_CAPS || (kc) == KC_INS || \
+     IS_OSM_SHIFT(kc))
+
+static uint16_t last_ctrl_tap = 0;
+static bool sticky_ctrl_active = false;
+static bool ctrl_waiting_for_release = false;
+
+static uint16_t last_alt_tap = 0;
+static bool sticky_alt_active = false;
+static bool alt_waiting_for_release = false;
+
+static uint16_t last_mod_activity = 0;
+
+// ─────────────── One-shot Modifier Tracking ───────────────
+static bool shift_was_active = false;
+static bool caps_was_active = false;
+static bool insert_was_active = false;
+
+static void clear_all_modifiers(void) {
+    unregister_mods(MOD_BIT(KC_RCTL) | MOD_BIT(KC_RALT) | MOD_BIT(KC_RSFT));
+    clear_oneshot_mods();
+    unregister_code(KC_CAPS);
+    unregister_code(KC_INS);
+
+    sticky_ctrl_active = false;
+    sticky_alt_active = false;
+    ctrl_waiting_for_release = false;
+    alt_waiting_for_release = false;
+    shift_was_active = false;
+    caps_was_active = false;
+    insert_was_active = false;
+
+    uprintf(">> TIMEOUT\n");
+}
+
+void matrix_scan_user(void) {
+    uint8_t osm_mods = get_oneshot_mods();
+
+    if (osm_mods & MOD_BIT(KC_RSFT)) {
+        if (!shift_was_active) {
+            shift_was_active = true;
+            last_mod_activity = timer_read();
+            uprintf(">> Shift ACTIVATED\n");
+        }
+    } else if (shift_was_active) {
+        shift_was_active = false;
+        uprintf(">> Shift RELEASED\n");
+    }
+
+    if ((sticky_ctrl_active || sticky_alt_active || shift_was_active || caps_was_active || insert_was_active) &&
+        timer_elapsed(last_mod_activity) > MODIFIER_TIMEOUT) {
+        clear_all_modifiers();
+    }
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    uint16_t now = timer_read();
+
+    if (!process_record_keychron_common(keycode, record)) {
+        return false;
+    }
+
+    if (IS_REAL_MOD(keycode)) {
+        last_mod_activity = now;
+    }
+
+    // Sticky CAPS
+    if (keycode == KC_CAPS) {
+        if (record->event.pressed) {
+            register_code(KC_CAPS);
+            caps_was_active = true;
+            last_mod_activity = now;
+            uprintf(">> Caps ACTIVATED\n");
+        }
+        return false;
+    }
+
+    // Sticky INSERT
+    if (keycode == KC_INS) {
+        if (record->event.pressed) {
+            register_code(KC_INS);
+            insert_was_active = true;
+            last_mod_activity = now;
+            uprintf(">> Insert ACTIVATED\n");
+        }
+        return false;
+    }
+
+    // Sticky Ctrl
+    if (keycode == KC_RCTL) {
+        if (record->event.pressed) {
+            if (timer_elapsed(last_ctrl_tap) < 500) {
+                register_mods(MOD_BIT(KC_RCTL));
+                sticky_ctrl_active = true;
+                uprintf(">> Ctrl ACTIVATED\n");
+            } else {
+                last_ctrl_tap = now;
+            }
+        }
+        return false;
+    }
+
+    // Sticky Alt
+    if (keycode == KC_RALT) {
+        if (record->event.pressed) {
+            if (timer_elapsed(last_alt_tap) < 500) {
+                register_mods(MOD_BIT(KC_RALT));
+                sticky_alt_active = true;
+                uprintf(">> Alt ACTIVATED\n");
+            } else {
+                last_alt_tap = now;
+            }
+        }
+        return false;
+    }
+
+    // Track whether to release sticky mods after non-mod key
+    if (record->event.pressed &&
+        !IS_REAL_MOD(keycode) &&
+        keycode != OSM(MOD_RSFT)) {
+        if (sticky_ctrl_active) ctrl_waiting_for_release = true;
+        if (sticky_alt_active) alt_waiting_for_release = true;
+    }
+
+    // Release all sticky mods (incl. SHIFT, CTRL, ALT, CAPS and INSERT) on next key release
+    if (!record->event.pressed &&
+        !IS_REAL_MOD(keycode) &&
+        keycode != OSM(MOD_RSFT) &&
+        !shift_was_active) {
+
+        if (ctrl_waiting_for_release) {
+            unregister_mods(MOD_BIT(KC_RCTL));
+            sticky_ctrl_active = false;
+            ctrl_waiting_for_release = false;
+            uprintf(">> Ctrl RELEASED\n");
+        }
+
+        if (alt_waiting_for_release) {
+            unregister_mods(MOD_BIT(KC_RALT));
+            sticky_alt_active = false;
+            alt_waiting_for_release = false;
+            uprintf(">> Alt RELEASED\n");
+        }
+
+        if (caps_was_active) {
+            unregister_code(KC_CAPS);
+            caps_was_active = false;
+            uprintf(">> Caps RELEASED\n");
+        }
+
+        if (insert_was_active) {
+            unregister_code(KC_INS);
+            insert_was_active = false;
+            uprintf(">> Insert RELEASED\n");
+        }
+    }
+
+    return true;
+}
