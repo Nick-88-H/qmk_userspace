0ef4768d6a3764398519318b6cec2b1220176d4d
Nic <169513221+Nick-88-H@users.noreply.github.com>
2025-06-16 11:15:33 +0800

    Create config.h

---
+++ keyboards/keychron/k5_max/ansi/white/keymaps/Nick88H/config.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#undef NKRO_ENABLE
+#undef FORCE_NKRO
+#define NKRO_ENABLE 0
+#define FORCE_NKRO OFF
+
+#define NO_ACTION_KEYBOARD   // nukes legacy fallback
+#define NO_MOUSEKEY
+#define NO_EXTRAKEY
+#define NO_MIDI
+#define NO_SERIAL
+#define NO_STENO
