6ad6ba9b12a8b8cc1683d0875faeb0d1cf378f89
Nic <169513221+Nick-88-H@users.noreply.github.com>
2025-06-16 11:18:53 +0800

    Update config.h

--- keyboards/keychron/k5_max/ansi/white/keymaps/Nick88H/config.h
+++ keyboards/keychron/k5_max/ansi/white/keymaps/Nick88H/config.h
@@ -5,7 +5,9 @@
 #define NKRO_ENABLE 0
 #define FORCE_NKRO OFF
 
-#define NO_ACTION_KEYBOARD   // nukes legacy fallback
+#define NO_ACTION_KEYBOARD  // disables fallback in action_util
+#define NO_USB_NKRO         // disables NKRO path in usb_main.c
+
 #define NO_MOUSEKEY
 #define NO_EXTRAKEY
 #define NO_MIDI
